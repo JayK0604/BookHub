239a775c412cec09001220f664174de65815a77e
jayku <jaykumarprajapati64@gmail.com>
2024-07-25 23:26:43 +0530

    updated Commit

--- app/src/main/java/com/monster/bookhub/activity/SplashActivity.kt
+++ app/src/main/java/com/monster/bookhub/activity/SplashActivity.kt
@@ -1,18 +1,18 @@
 package com.monster.bookhub.activity
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.view.KeyEvent
 import android.view.inputmethod.EditorInfo
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseApp
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.FirebaseApp
 import com.monster.bookhub.databinding.ActivitySplashBinding
 import com.monster.bookhub.model.User
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashBinding
